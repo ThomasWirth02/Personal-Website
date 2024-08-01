862627e562b7d22f6521a621a38efdd924bcc5cf
ThomasWirth02 <91318015+ThomasWirth02@users.noreply.github.com>
2024-08-01 22:10:37 +0200

    thing done

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.addEventListener('scroll', () => {
     
     document.getElementById("thomas-wirth").style.opacity = opacity1;
     document.getElementById("uh-1").style.opacity = opacity1;
-  
+    
 
 });
 
@@ -103,11 +103,17 @@ function updateHeilbronTime(){
 setInterval(updateHeilbronTime, 1000);
 updateHeilbronTime();
 
-const totalCards = 5; // Define the total number of cards
+const totalCards = 4; // Define the total number of cards
 let currentCard = 1;
+var isProcessing = false;
 
 document.getElementById('btn-next').addEventListener('click', () => {
-  if (currentCard < totalCards) {
+
+  if (currentCard < totalCards && !isProcessing) {
+    const btnPrev = document.getElementById('btn-prev');
+    const btnNext = document.getElementById('btn-next');
+
+    isProcessing = true;
     const cCard = document.getElementById('card' + currentCard);
     const nextCard = document.getElementById('card' + (currentCard + 1));
 
@@ -120,18 +126,31 @@ document.getElementById('btn-next').addEventListener('click', () => {
       cCard.removeEventListener('transitionend', handleTransitionEnd); 
       // Increment after the transition is complete
       currentCard++; 
+      isProcessing = false;
+      btnPrev.classList.remove('hidden');
+      if(currentCard == 4){
+        btnNext.classList.add('hidden');
+      }
     }
+    
+
 
     cCard.addEventListener('transitionend', handleTransitionEnd); 
   } else {
     // Handle reaching the last card (e.g., loop back, disable button)
     console.log("You've reached the last card!");
+    
   }
 });
 
 
 document.getElementById('btn-prev').addEventListener('click', () => {
-  if (currentCard > 1) {
+  if (currentCard > 1 && !isProcessing) {
+    const btnPrev = document.getElementById('btn-prev');
+    const btnNext = document.getElementById('btn-next');
+
+    isProcessing = true;
+
     const cCard = document.getElementById('card' + currentCard);
     const nextCard = document.getElementById('card' + (currentCard -1));
 
@@ -139,20 +158,51 @@ document.getElementById('btn-prev').addEventListener('click', () => {
 
     // Use a named function to remove the event listener cleanly
     function handleTransitionEnd() {
+      btnNext.classList.remove('hidden');
       nextCard.classList.remove('hidden');
       //nextCard.style.transform = 'translateX(0)';
       cCard.removeEventListener('transitionend', handleTransitionEnd); 
       // Increment after the transition is complete
       currentCard--; 
+      isProcessing = false;
+      if(currentCard == 1){
+        btnPrev.classList.add('hidden');
+      }
+
+
     }
+
 
     cCard.addEventListener('transitionend', handleTransitionEnd); 
   } else {
     // Handle reaching the last card (e.g., loop back, disable button)
     console.log("You've reached the last card!");
+    
   }
 });
 
 
+const card1Flip = document.getElementById('card1');
+
+card1Flip.addEventListener('click', () => {
+  card1Flip.classList.toggle('is-flipped');
+});
+
+const card2Flip = document.getElementById('card2');
+
+card2Flip.addEventListener('click', () => {
+  card2Flip.classList.toggle('is-flipped');
+});
+
+const card3Flip = document.getElementById('card3');
+
+card3Flip.addEventListener('click', () => {
+  card3Flip.classList.toggle('is-flipped');
+});
+const card4Flip = document.getElementById('card4');
+
+card4Flip.addEventListener('click', () => {
+  card4Flip.classList.toggle('is-flipped');
+});
 
   
