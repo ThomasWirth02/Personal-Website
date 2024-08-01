39d833f907a4f3161b8699929b596ef8e1432a26
ThomasWirth02 <91318015+ThomasWirth02@users.noreply.github.com>
2024-08-01 20:27:39 +0200

    Updated everything

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ class TextScramble {
       for (let i = 0; i < length; i++) {
         const from = oldText[i] || ''
         const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 70)
-        const end = start + Math.floor(Math.random() * 70)
+        const start = Math.floor(Math.random() * 40)
+        const end = start + Math.floor(Math.random() * 40)
         this.queue.push({ from, to, start, end })
       }
       cancelAnimationFrame(this.frameRequest)
@@ -78,117 +78,90 @@ class TextScramble {
       return this.chars[Math.floor(Math.random() * this.chars.length)]
     }
   }
-  document.addEventListener('DOMContentLoaded', () => {
-    const el1 = document.getElementById("abme-1");
-    const fx1 = new TextScramble(el1);
-    const el2 = document.getElementById("abme-2");
-    const fx2 = new TextScramble(el2);
-    const el3 = document.getElementById("abme-3");
-    const fx3 = new TextScramble(el3);
-    const el4 = document.getElementById("abme-4");
-    const fx4 = new TextScramble(el4);
-    const el5 = document.getElementById("abme-5");
-    const fx5 = new TextScramble(el5);
-    
-    const observerOptions = {
-      root: null, // Use the viewport as the container
-      rootMargin: '0px',
-      threshold: 0.4 // Trigger when 10% of the element is visible
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          // The element is visible, trigger the text scramble
-            fx1.setText('Hi! My name is Thomas. I am a student originally from Australia, currently studying').then(() => {
-            });
-            fx2.setText('in Germany. I have a deep passion for Computer Science, particularly in the').then(() => {
-            });
-            fx3.setText('development and implementation of projects. Seeing my work come to fruition is').then(() => {
-            });
-            fx4.setText('what drives me to pursue this career. I am hardworking and take great pride in').then(() => {
-            });
-            fx5.setText('spearheading projects, especially when I am deeply interested in the topic.').then(() => {
-            });
-          // Stop observing the element after the effect is triggered
-            observer.unobserve(entry.target);
-        }
-      });
-    }, observerOptions);
-
-    observer.observe(el1);
-    observer.observe(el2);
-
-    observer.observe(el3);
-
-    observer.observe(el4);
-
-    observer.observe(el5);
-
-  });
+  const phrases = [
+    'AUSTRALIAN STUDENT',
+    'STUDYING IN GERMANY'
+  ]
   
-  document.addEventListener('DOMContentLoaded', () => {
-    const el1 = document.getElementById("edu-info1");
-    const fx1 = new TextScramble(el1);
-    const el2 = document.getElementById("edu-info2");
-    const fx2 = new TextScramble(el2);
-    const el3 = document.getElementById("edu-info3");
-    const fx3 = new TextScramble(el3);
-    const el4 = document.getElementById("edu-info4");
-    const fx4 = new TextScramble(el4);
-    const el5 = document.getElementById("edu-info5");
-    const fx5 = new TextScramble(el5);
-    const el6 = document.getElementById("edu-info6");
-    const fx6 = new TextScramble(el6);
-    const el7 = document.getElementById("edu-info7");
-    const fx7 = new TextScramble(el7);
-    const el8 = document.getElementById("edu-info8");
-    const fx8 = new TextScramble(el8);
-    
-    const observerOptions = {
-      root: null, // Use the viewport as the container
-      rootMargin: '0px',
-      threshold: 0.4 // Trigger when 10% of the element is visible
-    };
-
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          // The element is visible, trigger the text scramble
-            fx1.setText('I am currently studying information engineering at the Technische Universität München.').then(() => {
-            });
-            fx2.setText('This Bachelor focuses on the theoretical aspect of Computer Science with courses').then(() => {
-            });
-            fx3.setText('complementing the theoretical side through the application of the given theory (primarily').then(() => {
-            });
-            fx4.setText('through the use of JAVA). In addition to the theoretical and practical courses we also').then(() => {
-            });
-            fx5.setText('have several courses which focus on the business side of developing and implementing a').then(() => {
-            });
-            fx6.setText('digital system.  This combination has enabled me to learn the full lifecycle of an').then(() => {
-            });
-            fx7.setText('information system, from the planning, development, implementation and then finally the').then(() => {
-            });
-            fx8.setText('maintenance involve with information system.').then(() => {
-            });
-          // Stop observing the element after the effect is triggered
-            observer.unobserve(entry.target);
-        }
-      });
-    }, observerOptions);
-
-    observer.observe(el1);
-    observer.observe(el2);
-
-    observer.observe(el3);
-
-    observer.observe(el4);
-
-    observer.observe(el5);
-    observer.observe(el6);
-    observer.observe(el7);
-    observer.observe(el8);
-  });
+  const el = document.getElementById('uh-1')
+  const fx = new TextScramble(el)
   
+  let counter = 0
+  const next = () => {
+    fx.setText(phrases[counter]).then(() => {
+      setTimeout(next, 1600)
+    })
+    counter = (counter + 1) % phrases.length
+  }
+  
+  next()
+  
+function updateHeilbronTime(){
+  const now = new Date();
+
+  const heilbronTime = new Intl.DateTimeFormat('en-GB',{
+    timeZone: 'Europe/Berlin',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+  }).format(now);
+  document.getElementById('heilbron-time').textContent = `${heilbronTime}`;
+
+}
+setInterval(updateHeilbronTime, 1000);
+updateHeilbronTime();
+
+const totalCards = 5; // Define the total number of cards
+let currentCard = 1;
+
+document.getElementById('btn-next').addEventListener('click', () => {
+  if (currentCard < totalCards) {
+    const cCard = document.getElementById('card' + currentCard);
+    const nextCard = document.getElementById('card' + (currentCard + 1));
+
+    cCard.classList.add('hidden');
+
+    // Use a named function to remove the event listener cleanly
+    function handleTransitionEnd() {
+      nextCard.classList.remove('hidden');
+      //nextCard.style.transform = 'translateX(0)';
+      cCard.removeEventListener('transitionend', handleTransitionEnd); 
+      // Increment after the transition is complete
+      currentCard++; 
+    }
+
+    cCard.addEventListener('transitionend', handleTransitionEnd); 
+  } else {
+    // Handle reaching the last card (e.g., loop back, disable button)
+    console.log("You've reached the last card!");
+  }
+});
+
+
+document.getElementById('btn-prev').addEventListener('click', () => {
+  if (currentCard > 1) {
+    const cCard = document.getElementById('card' + currentCard);
+    const nextCard = document.getElementById('card' + (currentCard -1));
+
+    cCard.classList.add('hidden');
+
+    // Use a named function to remove the event listener cleanly
+    function handleTransitionEnd() {
+      nextCard.classList.remove('hidden');
+      //nextCard.style.transform = 'translateX(0)';
+      cCard.removeEventListener('transitionend', handleTransitionEnd); 
+      // Increment after the transition is complete
+      currentCard--; 
+    }
+
+    cCard.addEventListener('transitionend', handleTransitionEnd); 
+  } else {
+    // Handle reaching the last card (e.g., loop back, disable button)
+    console.log("You've reached the last card!");
+  }
+});
+
+
 
   
